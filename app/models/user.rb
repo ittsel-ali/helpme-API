5ce85252fbbf402e7de31e06c3b72b147412628c
ittsel-ali <ittsel1@gmail.com>
2018-10-26 20:14:25 -0400

    project configurations and mobile user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+	class << self
+		def authenticate(email, password)
+	    user = User.find_for_database_authentication(:email => email)
+	    
+	    if user&.valid_for_authentication? { user.valid_password?(password) } && user&.active_for_authentication?
+	      user
+	    end
+	   end
+ 	end
+end
